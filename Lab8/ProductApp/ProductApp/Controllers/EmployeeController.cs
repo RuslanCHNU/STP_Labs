53746f588e7ecc34ded478e3901e73f2a22f2cf6
RuslanCHNU <viknianskyi.ruslan@chnu.edu.ua>
2025-05-19 18:02:23 +0300

    Виконав Лабораторну роботу 8

---
+++ Lab8/ProductApp/ProductApp/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Web.Http;
+using ProductApp.Models;
+using ProductApp.Repository;
+
+namespace ProductApp.Controllers
+{
+    [RoutePrefix("api/Employee")]
+    public class EmployeeController : ApiController
+    {
+        private readonly EmployeeRepository _empRepo = new EmployeeRepository();
+
+        // GET: api/Employee/GetAllEmpDetails
+        [HttpGet]
+        [Route("GetAllEmpDetails")]
+        public IHttpActionResult GetAllEmpDetails()
+        {
+            try
+            {
+                var employees = _empRepo.GetAllEmployees();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // POST: api/Employee/AddEmployee
+        [HttpPost]
+        [Route("AddEmployee")]
+        public IHttpActionResult AddEmployee([FromBody] EmployeeModel emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            bool added = _empRepo.AddEmployee(emp);
+            if (added)
+            {
+                return Ok("Employee added successfully");
+            }
+            else
+            {
+                return InternalServerError(new Exception("Failed to add employee"));
+            }
+        }
+
+        // PUT: api/Employee/EditEmpDetails/5
+        [HttpPut]
+        [Route("EditEmpDetails/{id}")]
+        public IHttpActionResult EditEmpDetails(int id, [FromBody] EmployeeModel emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            emp.Empid = id;
+            bool updated = _empRepo.UpdateEmployee(emp);
+            if (updated)
+            {
+                return Ok("Employee updated successfully");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE: api/Employee/DeleteEmp/5
+        [HttpDelete]
+        [Route("DeleteEmp/{id}")]
+        public IHttpActionResult DeleteEmp(int id)
+        {
+            bool deleted = _empRepo.DeleteEmployee(id);
+            if (deleted)
+            {
+                return Ok("Employee deleted successfully");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Lab8/ProductApp/ProductApp/Repository/EmployeeRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using ProductApp.Models;
+
+namespace ProductApp.Repository
+{
+    public class EmployeeRepository
+    {
+        private SqlConnection con;
+
+        private void Connection()
+        {
+            var constr = ConfigurationManager.ConnectionStrings["GetConn"].ToString();
+            con = new SqlConnection(constr);
+        }
+
+        public bool AddEmployee(EmployeeModel obj)
+        {
+            Connection();
+            SqlCommand com = new SqlCommand("AddNewEmpDetails", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            return i > 0;
+        }
+
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            Connection();
+            List<EmployeeModel> empList = new List<EmployeeModel>();
+
+            SqlCommand com = new SqlCommand("GetEmployees", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                empList.Add(new EmployeeModel
+                {
+                    Empid = Convert.ToInt32(dr["Id"]),
+                    Name = Convert.ToString(dr["Name"]),
+                    City = Convert.ToString(dr["City"]),
+                    Address = Convert.ToString(dr["Address"])
+                });
+            }
+            return empList;
+        }
+
+        public bool UpdateEmployee(EmployeeModel obj)
+        {
+            Connection();
+            SqlCommand com = new SqlCommand("UpdateEmpDetails", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            com.Parameters.AddWithValue("@EmpId", obj.Empid);
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            return i > 0;
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            Connection();
+            SqlCommand com = new SqlCommand("DeleteEmpById", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            com.Parameters.AddWithValue("@EmpId", id);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            return i > 0;
+        }
+    }
+}
